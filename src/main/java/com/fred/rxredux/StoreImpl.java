47696f79e23daa249599ca86e2a44b3041fe6fae
Fred <fgoncalves@babbel.com>
2016-07-25 18:22:19 +0200

    Weird stuff with compose
    Signed-off-by: Fred <fgoncalves@babbel.com>

--- src/main/java/com/fred/rxredux/StoreImpl.java
+++ src/main/java/com/fred/rxredux/StoreImpl.java
@@ -4,11 +4,9 @@ import com.fred.rxredux.transformers.IOToIOSchedulerTransformer;
 import com.fred.rxredux.transformers.SchedulerTransformer;
 import java.util.ArrayList;
 import java.util.List;
-import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.functions.Func1;
-import rx.functions.Func2;
 import rx.subjects.PublishSubject;
 
 /**
@@ -18,48 +16,23 @@ import rx.subjects.PublishSubject;
  * @param <A> Action's class
  */
 public class StoreImpl<S extends State, A extends Action> implements Store<S, A> {
-  private Func2<A, S, S> mainDispatcher = new Func2<A, S, S>() {
-    public S call(A a, S s) {
-      return rootReducer.call(a, s);
+  // Curried dispatcher -> calls root reducer
+  private Func1<A, Func1<S, S>> mainDispatcher = new Func1<A, Func1<S, S>>() {
+    public Func1<S, S> call(final A a) {
+      return new Func1<S, S>() {
+        public S call(S s) {
+          return rootReducer.call(a, s);
+        }
+      };
     }
   };
 
   private final PublishSubject<S> stateSubject = PublishSubject.create();
-  private final List<Middleware<S, A>> middlewares;
+  private final List<Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>>> middlewares;
   private final Reducer<S, A> rootReducer;
   private final SchedulerTransformer actionStreamSchedulerTransformer;
   private final SchedulerTransformer subscriptionSchedulerTransformer;
   private S currentState;
-
-  private Func1<A, Func1<A, S>> compose(final List<Func1<A, S>> toCompose) {
-    if (toCompose.isEmpty()) {
-      throw new IllegalStateException("Compose cannot be called with an empty list");
-    }
-
-    if (toCompose.size() == 1) {
-      return new Func1<A, Func1<A, S>>() {
-        public Func1<A, S> call(A a) {
-          return toCompose.get(0);
-        }
-      };
-    }
-
-    Func1<A, Func1<A, S>> result = new Func1<A, Func1<A, S>>() {
-      public Func1<A, S> call(A a) {
-        return toCompose.get(0);
-      }
-    };
-    for (Func1<A, S> func : toCompose) {
-      final Func1<A, Func1<A, S>> finalResult = result;
-      result = new Func1<A, Func1<A, S>>() {
-        public Func1<A, S> call(A a) {
-          return finalResult.call(a);
-        }
-      };
-    }
-
-    return result;
-  }
 
   /**
    * Create a store with the default config. This will effectively create a store that will use the
@@ -74,7 +47,7 @@ public class StoreImpl<S extends State, A extends Action> implements Store<S, A>
    */
   public static <S extends State, A extends Action> Store<S, A> create(Reducer<S, A> rootReducer,
       S initialState, SchedulerTransformer subscriptionSchedulerTransformer,
-      List<Middleware<S, A>> middlewares) {
+      List<Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>>> middlewares) {
     return new StoreImpl<S, A>(rootReducer, initialState,
         new IOToIOSchedulerTransformer(),
         subscriptionSchedulerTransformer, middlewares);
@@ -87,12 +60,14 @@ public class StoreImpl<S extends State, A extends Action> implements Store<S, A>
       S initialState, SchedulerTransformer subscriptionSchedulerTransformer) {
     return new StoreImpl<S, A>(rootReducer, initialState,
         new IOToIOSchedulerTransformer(),
-        subscriptionSchedulerTransformer, new ArrayList<Middleware<S, A>>());
+        subscriptionSchedulerTransformer,
+        new ArrayList<Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>>>());
   }
 
   public StoreImpl(Reducer<S, A> rootReducer, S initialState,
       SchedulerTransformer actionStreamSchedulerTransformer,
-      SchedulerTransformer subscriptionSchedulerTransformer, List<Middleware<S, A>> middlewares) {
+      SchedulerTransformer subscriptionSchedulerTransformer,
+      List<Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>>> middlewares) {
     this.rootReducer = rootReducer;
     this.actionStreamSchedulerTransformer = actionStreamSchedulerTransformer;
     this.currentState = initialState;
@@ -101,28 +76,9 @@ public class StoreImpl<S extends State, A extends Action> implements Store<S, A>
   }
 
   public void dispatch(final A action) {
-
-    Observable.from(middlewares).flatMap(new Func1<Middleware<S, A>, Observable<S>>() {
-      public Observable<S> call(Middleware<S, A> middleware) {
-        return middleware.apply(StoreImpl.this, currentState, action);
-      }
-    }).mergeWith(rootReducer.reduce(action, currentState))
-        .last()
-        .compose(actionStreamSchedulerTransformer.<S>applySchedulers())
-        .subscribe(new Subscriber<S>() {
-          public void onCompleted() {
-
-          }
-
-          public void onError(Throwable throwable) {
-            stateSubject.onError(throwable);
-          }
-
-          public void onNext(S s) {
-            currentState = s;
-            stateSubject.onNext(s);
-          }
-        });
+    // TODO: encapsulate middleware
+    currentState = mainDispatcher.call(action).call(state());
+    stateSubject.onNext(currentState);
   }
 
   public Subscription subscribe(Subscriber<S> stateSubscriber) {
@@ -133,5 +89,33 @@ public class StoreImpl<S extends State, A extends Action> implements Store<S, A>
 
   public S state() {
     return currentState;
+  }
+
+  /**
+   * Compose the different middlewares together with the main dispatcher
+   *
+   * @param toCompose The middlewares to compose
+   * @return The composed function
+   */
+  private Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>> compose(
+      final List<Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>>> toCompose) {
+    if (toCompose.isEmpty()) {
+      throw new IllegalStateException("Compose cannot be called with an empty list");
+    }
+
+    Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>> result = toCompose.get(0);
+
+    if (toCompose.size() == 1) return result;
+
+    for (final Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>> func : toCompose) {
+      final Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>> finalResult = result;
+      result = new Func1<Func1<A, Func1<S, S>>, Func1<A, Func1<S, S>>>() {
+        public Func1<A, Func1<S, S>> call(Func1<A, Func1<S, S>> next) {
+          return func.call(finalResult.call(next));
+        }
+      };
+    }
+
+    return result;
   }
 }
